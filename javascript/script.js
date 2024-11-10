f1b3aa77698d6f0635ee52e79a418d97eadc5a8e
salgadoluciana <luciana.salgado@fatec.sp.gov.br>
2024-11-09 21:31:01 -0300

    Ajustes no css da galeria

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,15 @@
+// Filtro de Galeria
 function filterGallery(category) {
-    // Seleciona todos os itens da galeria
+    // Seleciona todos os itens da galeria e botões de filtro
     const items = document.querySelectorAll('.gallery-item');
-    const buttons = document.querySelectorAll('.filter-btn'); // Seleciona todos os botões de filtro
-    
-    // Limpa a classe 'active' de todos os botões
-    buttons.forEach(button => {
-        button.classList.remove('active');
-    });
-    
-    // Adiciona a classe 'active' ao botão clicado
-    const activeButton = document.querySelector(`.filter-btn[data-category="${category}"]`);
-    if (activeButton) {
-        activeButton.classList.add('active');
-    }
+    const buttons = document.querySelectorAll('.filter-btn');
 
-    // Exibe ou oculta as imagens com base na categoria selecionada
+    // Remove a classe 'active' de todos os botões e adiciona no botão clicado
+    buttons.forEach(button => button.classList.remove('active'));
+    const activeButton = document.querySelector(`.filter-btn[data-category="${category}"]`);
+    if (activeButton) activeButton.classList.add('active');
+
+    // Exibe ou oculta as imagens com base na categoria
     items.forEach(item => {
         if (item.getAttribute('data-category') === category || category === 'todos') {
             item.style.display = 'block';
@@ -22,4 +17,29 @@ function filterGallery(category) {
             item.style.display = 'none';
         }
     });
+}
+
+// Modal de Visualização e Navegação
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.gallery-item img');
+
+function openModal(index) {
+    document.getElementById("imageModal").style.display = "block";
+    showImage(index);
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+function showImage(index) {
+    const modalImage = document.getElementById("modalImage");
+    if (index >= images.length) currentImageIndex = 0;
+    else if (index < 0) currentImageIndex = images.length - 1;
+    else currentImageIndex = index;
+    modalImage.src = images[currentImageIndex].src;
+}
+
+function changeImage(step) {
+    showImage(currentImageIndex + step);
 }
