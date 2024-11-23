6270d1a5729b76622d839654c828871a6768dee3
lucas-oliveira437 <lucas.oliveira437@fatec.sp.gov.br>
2024-11-23 02:24:57 -0300

    Busca pelo ViaCEP incluída

---
+++ scripts/cadastro/cadastro.js
@@ -0,0 +1,43 @@
+function buscar() {
+    const cep = document.getElementById("cep").value;
+    alert(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        response.json().then(data => {
+            console.log(data)
+        })
+    })
+
+}
+
+function enviar() {
+    let endereco = document.getElementById("endereco").value;
+    let bairro = document.getElementById("bairro").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+
+    let json = {
+        "endereco": endereco,
+        "bairro": bairro,
+        "cidade": cidade,
+        "estado": estado
+    }
+};
+
+const cep = document.getElementById("cep");
+cep.addEventListener("blur", (e) => {
+    let Cep = document.getElementById("cep").value;
+    let search = Cep.replace("-", "");
+    fetch(`https://viacep.com.br/ws/${search}/json/`).then((response) => {
+        response.json().then(data => {
+            console.log(data);
+            document.getElementById("endereco").value = data.logradouro;
+            document.getElementById("bairro").value = data.bairro;
+            document.getElementById("cidade").value = data.localidade;
+            document.getElementById("estado").value = data.uf;
+        })
+    })
+})
+
+
+
+
